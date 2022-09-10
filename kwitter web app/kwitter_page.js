2cb4d267f6f683fbacd29ff1ff8a2d48e754dd9a
RRKING13 <82946348+RRKING13@users.noreply.github.com>
2022-09-10 14:42:22 +0530

    Add files via upload

---
+++ kwitter web app/kwitter_page.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD1rRvZepJuIhCNR--Xkh7oPC-_WCoaLcs",
+    authDomain: "lets-chat-web-app-263b6.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-263b6-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-263b6",
+    storageBucket: "lets-chat-web-app-263b6.appspot.com",
+    messagingSenderId: "139212177026",
+    appId: "1:139212177026:web:e077f8850a0c097a53be9b"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  
